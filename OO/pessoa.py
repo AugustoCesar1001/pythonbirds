f6cad8ecf3214a011b7da20405a1b3196eee2afa
Augusto <augustoscesars@hotmail.com>
2020-08-11 23:33:03 -0300

    Criado classe Pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def cumprimentar(self, nome=None, idade=0):
+        self.idade = idade
+        self.nome = nome
+        return f'Olá {nome, idade}'
+
+if __name__ == '__main__':
+    nome = 'Augusto'
+    idade = 26
+    p = Pessoa()
+    print(id(p))
+    print(p.cumprimentar(nome, idade))
